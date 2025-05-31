02dfbd547d60b302200772c153a4fd474e910f43
Doni Nurramdan <doni@weplus.id>
2025-05-31 11:08:23 +0700

    [admin] : disable add menu

--- page-prime/app/Views/pages-admin/menu.php
+++ page-prime/app/Views/pages-admin/menu.php
@@ -4,7 +4,7 @@
 <div class="container">
     <h2>Managemen Menu</h2>
     <div style="text-align: right;">
-        <button style="margin: 15px 0px 15px 0px;" class="btn btn-success">Tambah Menu</button>
+        <!-- <button style="margin: 15px 0px 15px 0px;" class="btn btn-success" data-toggle="modal" data-target="#addData" onclick="setForm()">Tambah Menu</button> -->
     </div>
     <table id="example" class="table table-striped table-bordered">
         <thead>
@@ -71,6 +71,125 @@
         .catch(error => {
             console.error('Error:', error.message);
         });
+
+    
+    function setForm(){
+        $("#titleData").html("Tambah Menu");
+        let form = ``;
+        form += `
+        <div class="form-group">
+        <label for="lang">Bahasa</label>
+        <select class="form-control" id="lang">
+        </select>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="name">Nama Menu</label>
+        <input type="text" class="form-control" id="name" aria-describedby="name">
+        <span id="translated_data"></span>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="name">Parent Menu</label>
+        <select class="form-control" id="parent_id">
+        </div>
+        `;
+        form += `
+        <div">
+            <a class="btn btn-secondary" id="translate">Translate</a>
+        </div>
+        `;
+        form += `
+        <div class="d-flex justify-content-end">
+            <a class="btn btn-success" id="tambah">Tambah</a>
+        </div>
+        `;
+
+        $("#formData").html(form);
+        // get api language
+        fetch(apiURL + '/api/language')
+            .then(response => response.json())
+            .then(data => {
+                let languageList = '';
+                $.each(data.data, function(index, item) {
+                    languageList += `<option value="`+item.sort_name+`">`+item.translate_id+'-'+item.name+`</option>`;
+                });
+    
+                $("#lang").html(languageList);
+            })
+            .catch(error => {
+                console.error('Error:', error.message);
+            });
+    }
+
+    document.addEventListener("click", function(e) {
+        if (e.target && e.target.id === "tambah") {
+
+            let translate_data = $("#translated_data").text()
+            let names = "";
+            if(translate_data == ""){
+                names = $("#name").val()
+            }else{
+                names = translate_data
+            }
+            let body = {
+                lang:$("#lang").val(),
+                parent_id:$("#parent_id").val(),
+                name:names
+            }
+            console.log(body);
+
+            fetch(apiURL + '/admin/menu', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer '+token,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Tambah Data berhasil:', data);
+                alert('Berhasil Menambah Data!');
+                window.location.href = "/admin/menu";
+            })
+            .catch(error => {
+                console.error('Tambah Data gagal:', error);
+                alert('Gagal Menambah Data!');
+            });
+        }else if(e.target && e.target.id === "translate"){
+            let text = $("#lang option:selected").text();
+            let split = text.split('-');
+            if(split[0] != ''){
+                console.log(text);
+
+                let body = {
+                    textData:$("#name").val(),
+                    from:"id",
+                    to:split[0]
+                }
+                fetch(apiURL + '/admin/translate', {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': 'Bearer '+token,
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(body)
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Translate berhasil:', data.data);
+                    $("#translated_data").text(data.data[0])
+                })
+                .catch(error => {
+                    console.error('Translate gagal:', error);
+                    alert('Gagal Translate Data!');
+                });
+            }
+        }
+    });
 </script>
 
 <?= $this->endSection() ?>
