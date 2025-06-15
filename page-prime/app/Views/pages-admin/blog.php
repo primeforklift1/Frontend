89fffea11c19a4bb8ebd8d984afb031c3d13a0b9
Doni Nurramdan <doni@weplus.id>
2025-06-15 11:58:17 +0700

    [crud] : update crud blog

--- page-prime/app/Views/pages-admin/blog.php
+++ page-prime/app/Views/pages-admin/blog.php
@@ -10,9 +10,11 @@
         <thead>
             <tr>
                 <th>No</th>
+                <th>Bahasa</th>
                 <th>Gambar</th>
                 <th>Judul Blog</th>
                 <td>Preface</td>
+                <td>Status</td>
                 <th>Aksi</th>
             </tr>
         </thead>
@@ -20,9 +22,11 @@
         <tfoot>
             <tr>
                 <th>No</th>
+                <th>Bahasa</th>
                 <th>Gambar</th>
                 <th>Judul Blog</th>
                 <td>Preface</td>
+                <td>Status</td>
                 <th>Aksi</th>
             </tr>
         </tfoot>
@@ -32,7 +36,7 @@
 </div>
 <script>
     // get api info all page
-    fetch(apiURL + '/api/blog/where', {
+    fetch(apiURL + '/api/blog-ori/where', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -47,10 +51,15 @@
             $.each(data.data, function(index, item) {
                 infoList += `<tr>
                                     <td>` + (index + 1) + `</td>
+                                    <td>` + item.lang + `</td>
                                     <td><img width="100" src="`+baseUrl+item.image+`" alt=""></td>
                                     <td>` + item.title + `</td>
                                     <td>` + item.preface + `</td>
-                                    <td>Edit | Hapus</td>
+                                    <td>`+(item.status == 1 ? 'Aktif' : 'Non Aktif')+`</td>
+                                    <td>
+                                    <button class="btn btn-primary" data-toggle="modal" data-target="#addData" onclick="setFormUpdate(`+item.id+`)">Edit</button>
+                                    <button class="btn btn-danger" onclick="if(confirm('Yakin ingin menghapus ini?')) hapus(` + item.id + `)">Hapus</button>
+                                    </td>
                                 </tr>`;
             });
 
@@ -65,6 +74,7 @@
         $("#titleData").html("Tambah Blog");
         let form = ``;
         form += `
+        <input type="hidden" id="idData">
         <div class="form-group">
         <label for="lang">Bahasa</label>
         <select class="form-control" id="lang">
@@ -314,6 +324,239 @@
                     alert('Gagal Translate Data!');
                 });
             }
+    }
+
+    function setFormUpdate(id){
+        $("#titleData").html("Edit Blog");
+        let form = ``;
+        form += `
+        <input type="hidden" id="idData">
+        <div class="form-group">
+        <label for="lang">Bahasa</label>
+        <select class="form-control" id="lang">
+        </select>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="translated_data_title">Judul Blog</label>
+        <input type="text" class="form-control" id="translated_data_title" aria-describedby="translated_data_title" placeholder="">
+        <span id="val_translated_data_title"></span>
+        </div>
+        `;
+        form += `
+        <div">
+            <a class="btn btn-secondary" onclick="translator('translated_data_title')">Translate</a>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="translated_data_preface">Preface</label>
+        <div style="height: 200px;border-radius: 0px 0px 10px 10px;" id="translated_data_preface"></div>
+        <span id="val_translated_data_preface"></span>
+        </div>
+        `;
+        form += `
+        <div">
+            <a class="btn btn-secondary" onclick="translator('translated_data_preface')">Translate</a>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="translated_data_value">Detail</label>
+        <div style="height: 200px;border-radius: 0px 0px 10px 10px;" id="translated_data_value"></div>
+        <span id="val_translated_data_value"></span>
+        </div>
+        `;
+        form += `
+        <div">
+            <a class="btn btn-secondary" onclick="translator('translated_data_value')">Translate</a>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="keywords">Keywords</label>
+        <input type="text" class="form-control" id="keywords" aria-describedby="keywords" placeholder="">
+        <span id="val_keywords"></span>
+        </div>
+        `;
+        form += `
+        <div">
+            <a class="btn btn-secondary" onclick="translator('keywords')">Translate</a>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="flag">Gambar</label>
+        <input type="file" class="form-control" id="flag" aria-describedby="flag" name="flag">
+        <label for="filedata">File Uploaded : </label>
+        <span id="flag_image"></span>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="status">Status</label>
+        <select class="form-control" id="status">
+        <option value="0">Non Aktif</option>
+        <option value="1">Aktif</option>
+        </select>
+        </div>
+        `;
+        form += `
+        <div class="d-flex justify-content-end">
+            <a class="btn btn-success" id="simpan">Simpan</a>
+        </div>
+        `;
+        $("#formData").html(form);
+        // get api language
+        fetch(apiURL + '/api/language')
+            .then(response => response.json())
+            .then(data => {
+                let languageList = '';
+                $.each(data.data, function(index, item) {
+                    languageList += `<option value="`+item.sort_name+`">`+item.translate_id+'-'+item.name+`</option>`;
+                });
+    
+                $("#lang").html(languageList);
+                // get api blog by id
+                fetch(apiURL + '/api/blog-ori/'+id)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data);
+                        $("#idData").val(data.data.id);
+                        $("#lang").val(data.data.lang);
+                        $("#translated_data_title").val(data.data.title);
+                        $("#translated_data_preface").html(data.data.preface);
+                        $("#translated_data_value").html(data.data.detail);
+                        $("#keywords").val(data.data.keyword);
+                        $("#flag_image").html(data.data.image);
+                        $("#status").val(data.data.status);
+
+                        const toolbarOptions = [
+                            [{
+                                'font': []
+                            }],
+
+                            ['bold', 'italic'],
+                            ['link'],
+                            ['code-block'],
+                            [{
+                                'list': 'ordered'
+                            }, {
+                                'list': 'bullet'
+                            }, {
+                                'list': 'check'
+                            }],
+                            ['image']
+                        ];
+                        const quill2 = new Quill('#translated_data_preface', {
+                            modules: {
+                                toolbar: toolbarOptions
+                            },
+                            theme: 'snow'
+                        });
+                        const quill = new Quill('#translated_data_value', {
+                            modules: {
+                                toolbar: toolbarOptions
+                            },
+                            theme: 'snow'
+                        });
+                    })
+                    .catch(error => {
+                        console.error('Error:', error.message);
+                    });
+            })
+            .catch(error => {
+                console.error('Error:', error.message);
+            });
+
+    }
+
+    document.addEventListener("click", function(e) {
+        if (e.target && e.target.id === "simpan") {
+
+            let translate_data_title = $("#val_translated_data_title").text()
+            let translated_data_preface = $("#val_translated_data_preface").text()
+            let translated_data_value = $("#val_translated_data_value").text()
+            let keywords = $("#val_keywords").text()
+            let config_name = "";
+            let config_preface = "";
+            let config_value = "";
+            let config_keywords = "";
+            if(translate_data_title == ""){
+                config_name = $("#translated_data_title").val()
+            }else{
+                config_name = translate_data_title
+            }
+            if(translated_data_preface == ""){
+                config_preface = $("#translated_data_preface"+ " .ql-editor").html()
+            }else{
+                config_preface = translated_data_preface
+            }
+            if(translated_data_value == ""){
+                config_value = $("#translated_data_value"+ " .ql-editor").html();
+            }else{
+                config_value = translated_data_value
+            }
+            if(keywords == ""){
+                config_keywords = $("#keywords").val()
+            }else{
+                config_keywords = keywords
+            }
+            let body = {
+                id:$("#idData").val(),
+                lang:$("#lang").val(),
+                title:config_name,
+                slug:"",
+                preface:config_preface,
+                detail:config_value,
+                keyword:config_keywords,
+                image:$("#flag_image").text(),
+                status:$("#status").val()
+            }
+            console.log(body);
+            // put api blog by id
+            fetch(apiURL + '/admin/blog', {
+                method: 'PUT',
+                headers: {
+                    'Authorization': 'Bearer '+token,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Edit Data berhasil:', data);
+                alert('Berhasil Edit Data!');
+                window.location.href = "/admin/blog";
+            })
+            .catch(error => {
+                console.error('Edit Data gagal:', error);
+                alert('Gagal Edit Data!');
+            });
+        }
+    });
+
+    function hapus(id){
+        console.log(id);
+        // delete api blog by id
+        fetch(apiURL + '/admin/blog/'+id, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': 'Bearer '+token,
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Hapus Data berhasil:', data);
+            alert('Berhasil Hapus Data!');
+            window.location.href = "/admin/blog";
+        })
+        .catch(error => {
+            console.error('Hapus Data gagal:', error);
+            alert('Gagal Hapus Data!');
+        });
     }
 </script>
 
