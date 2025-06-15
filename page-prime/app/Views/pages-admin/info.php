1e43c4355a6a2a7958318e4f01ab1291f9ea3429
Doni Nurramdan <doni@weplus.id>
2025-06-15 12:00:06 +0700

    [crud] : update crud info

--- page-prime/app/Views/pages-admin/info.php
+++ page-prime/app/Views/pages-admin/info.php
@@ -10,9 +10,11 @@
         <thead>
             <tr>
                 <th>No</th>
+                <th>Bahasa</th>
                 <th>Info Type</th>
                 <th>Nama Info</th>
                 <th>Gambar</th>
+                <th>Status</th>
                 <th>Aksi</th>
             </tr>
         </thead>
@@ -20,9 +22,11 @@
         <tfoot>
             <tr>
                 <th>No</th>
+                <th>Bahasa</th>
                 <th>Info Type</th>
                 <th>Nama Info</th>
                 <th>Gambar</th>
+                <th>Status</th>
                 <th>Aksi</th>
             </tr>
         </tfoot>
@@ -47,10 +51,15 @@
             $.each(data.data, function(index, item) {
                 infoList += `<tr>
                                     <td>` + (index + 1) + `</td>
+                                    <td>` + item.lang + `</td>
                                     <td>` + item.config_type + `</td>
                                     <td>` + item.config_name + `</td>
                                     <td><img width="100" src="`+baseUrl+item.image+`" alt=""></td>
-                                    <td>Edit | Hapus</td>
+                                    <td>`+(item.status == 1 ? 'Aktif' : 'Non Aktif')+`</td>
+                                    <td>
+                                    <button class="btn btn-primary" data-toggle="modal" data-target="#addData" onclick="setFormUpdate(`+item.config_id+`)">Edit</button>
+                                    <button class="btn btn-danger" onclick="if(confirm('Yakin ingin menghapus ini?')) hapus(` + item.config_id + `)">Hapus</button>
+                                    </td>
                                 </tr>`;
             });
 
@@ -289,6 +298,210 @@
             }
     }
 
+    function setFormUpdate(id){
+        $("#titleData").html("Edit Info");
+        let form = ``;
+        form += `
+        <input type="hidden" id="idData">
+        <div class="form-group">
+        <label for="lang">Bahasa</label>
+        <select class="form-control" id="lang">
+        </select>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="configType">Tipe Info</label>
+        <select class="form-control" id="configType">
+            <option value="about">Tentang</option>
+            <option value="contact">Kontak/ Alamat</option>
+            <option value="market">Marketplace</option>
+            <option value="soc">Sosial Media</option>
+        </select>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="translated_data_title">Nama Judul</label>
+        <input type="text" class="form-control" id="translated_data_title" aria-describedby="translated_data_title" placeholder="Visi">
+        <span id="val_translated_data_title"></span>
+        </div>
+        `;
+        form += `
+        <div">
+            <a class="btn btn-secondary" onclick="translator('translated_data_title')">Translate</a>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="translated_data_value">Informasi</label>
+        <div style="height: 200px;border-radius: 0px 0px 10px 10px;" id="translated_data_value"></div>
+        <span id="val_translated_data_value"></span>
+        </div>
+        `;
+        form += `
+        <div">
+            <a class="btn btn-secondary" onclick="translator('translated_data_value')">Translate</a>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="order">Urutan</label>
+        <input type="text" class="form-control" id="order" aria-describedby="order">
+        <span id="translated_data"></span>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="flag">Gambar</label>
+        <input type="file" class="form-control" id="flag" aria-describedby="flag" name="flag">
+        <label for="filedata">File Uploaded : </label>
+        <span id="flag_image"></span>
+        </div>
+        `;
+        form += `
+        <div class="form-group">
+        <label for="status">Status</label>
+        <select class="form-control" id="status">
+        <option value="0">Non Aktif</option>
+        <option value="1">Aktif</option>
+        </select>
+        </div>
+        `;
+        form += `
+        <div class="d-flex justify-content-end">
+            <a class="btn btn-success" id="simpan">Simpan</a>
+        </div>
+        `;
+        $("#formData").html(form);
+        // get api language
+        fetch(apiURL + '/api/language')
+            .then(response => response.json())
+            .then(data => {
+                let languageList = '';
+                $.each(data.data, function(index, item) {
+                    languageList += `<option value="`+item.sort_name+`">`+item.translate_id+'-'+item.name+`</option>`;
+                });
+    
+                $("#lang").html(languageList);
+                // get api config by id
+                fetch(apiURL + '/api/config/'+id)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data);
+                        $("#idData").val(data.data.config_id);
+                        $("#lang").val(data.data.lang);
+                        $("#configType").val(data.data.config_type);
+                        $("#translated_data_title").val(data.data.config_name);
+                        $("#translated_data_value").html(data.data.config_value);
+                        $("#order").val(data.data.order);
+                        $("#flag_image").html(data.data.image);
+                        $("#status").val(data.data.status);
+
+                        const toolbarOptions = [
+                            [{
+                                'font': []
+                            }],
+
+                            ['bold', 'italic'],
+                            ['link'],
+                            ['code-block'],
+                            [{
+                                'list': 'ordered'
+                            }, {
+                                'list': 'bullet'
+                            }, {
+                                'list': 'check'
+                            }],
+                            ['image']
+                        ];
+                        const quill = new Quill('#translated_data_value', {
+                            modules: {
+                                toolbar: toolbarOptions
+                            },
+                            theme: 'snow'
+                        });
+                    })
+                    .catch(error => {
+                        console.error('Error:', error.message);
+                    });
+            })
+            .catch(error => {
+                console.error('Error:', error.message);
+            });
+
+    }
+
+    document.addEventListener("click", function(e) {
+        if (e.target && e.target.id === "simpan") {
+
+            let translate_data_title = $("#val_translated_data_title").text()
+            let translated_data_value = $("#val_translated_data_value").text()
+            let config_name = "";
+            let config_value = "";
+            if(translate_data_title == ""){
+                config_name = $("#translated_data_title").val()
+            }else{
+                config_name = translate_data_title
+            }
+            if(translated_data_value == ""){
+                config_value = $("#translated_data_value"+ " .ql-editor").html();
+            }else{
+                config_value = translated_data_value
+            }
+            let body = {
+                id:$("#idData").val(),
+                lang:$("#lang").val(),
+                config_type:$("#configType").val(),
+                order:$("#order").val(),
+                config_name:config_name,
+                config_value:config_value,
+                image:$("#flag_image").text(),
+                status:$("#status").val()
+            }
+            // put api config by id
+            fetch(apiURL + '/admin/config', {
+                method: 'PUT',
+                headers: {
+                    'Authorization': 'Bearer '+token,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Edit Data berhasil:', data);
+                alert('Berhasil Edit Data!');
+                window.location.href = "/admin/info";
+            })
+            .catch(error => {
+                console.error('Edit Data gagal:', error);
+                alert('Gagal Edit Data!');
+            });
+        }
+    });
+
+    function hapus(id){
+        console.log(id);
+        // delete api config by id
+        fetch(apiURL + '/admin/config/'+id, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': 'Bearer '+token,
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Hapus Data berhasil:', data);
+            alert('Berhasil Hapus Data!');
+            window.location.href = "/admin/info";
+        })
+        .catch(error => {
+            console.error('Hapus Data gagal:', error);
+            alert('Gagal Hapus Data!');
+        });
+    }
     
 </script>
 
