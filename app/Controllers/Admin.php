e3ad32971550ef31aa9bcf3cd6fa52b3be49e457
Doni Nurramdan <nuramdandoni@gmail.com>
2025-10-08 09:01:47 +0700

    [upload] : issue path

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -75,7 +75,7 @@ class Admin extends BaseController
         $sourceUrl = $request->getPost('sourceUrl');
         $targetDir = FCPATH . $request->getPost('targetDir');
         $filename = basename($sourceUrl);
-        $targetPath = 'public/'.$targetDir . $filename;
+        $targetPath = $targetDir . $filename;
 
         if (!is_dir($targetDir)) {
             mkdir($targetDir, 0755, true);
